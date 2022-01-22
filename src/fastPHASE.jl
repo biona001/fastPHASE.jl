18242ecc9dd83b04ab990e1613c94be2a8308f03
Unknown <benchu99@hotmail.com>
2022-01-21 18:37:28 -0800

    add Pkg to dependency

--- src/fastPHASE.jl
+++ src/fastPHASE.jl
@@ -1,5 +1,17 @@
 module fastPHASE
 
-greet() = print("Hello World!")
+using Pkg.Artifacts
+
+export fastPHASE_EXE
+
+if Sys.isapple()
+    fastPHASE_EXE = joinpath(artifact"fastPHASE", "fastPHASE")
+    chmod(fastPHASE_EXE, 755)
+elseif Sys.islinux()
+    fastPHASE_EXE = joinpath(artifact"fastPHASE", "fastPHASE")
+    chmod(fastPHASE_EXE, 755)
+else
+    error("fastPHASE only supports Linux and MacOS")
+end
 
 end # module
