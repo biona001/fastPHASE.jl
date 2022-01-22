3899f62f60e1ba7f2af74e74b224098fc19ba67e
Unknown <benchu99@hotmail.com>
2022-01-21 19:36:46 -0800

    give fastPHASE exeecutable permission via run directly cuz chmod in julia seems buggy

--- src/fastPHASE.jl
+++ src/fastPHASE.jl
@@ -6,10 +6,10 @@ export fastPHASE_EXE
 
 if Sys.isapple()
     fastPHASE_EXE = joinpath(artifact"fastPHASE", "fastPHASE")
-    chmod(fastPHASE_EXE, 755)
+    run(`chmod +x $fastPHASE_EXE`) # fastPHASE executable by default is read-only
 elseif Sys.islinux()
     fastPHASE_EXE = joinpath(artifact"fastPHASE", "fastPHASE")
-    chmod(fastPHASE_EXE, 755)
+    run(`chmod +x $fastPHASE_EXE`) # fastPHASE executable by default is read-only
 else
     error("fastPHASE only supports Linux and MacOS")
 end
